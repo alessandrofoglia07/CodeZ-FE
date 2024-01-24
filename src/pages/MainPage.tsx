904a595de64b34f91dfac5b6b110674cdb2f0ad5
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-01-24 21:14:10 +0100

    Added orientation to stars floating in bg

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,15 +4,16 @@ interface StarEl {
     x: number;
     y: number;
     size: number;
+    orientation?: 'top-bottom' | 'bottom-top';
 }
 
-const Star = ({ x, y, size }: StarEl) => {
+const Star = ({ x, y, size, orientation = 'bottom-top' }: StarEl) => {
     const style: React.CSSProperties = {
         top: y,
         left: x,
         width: size,
         height: size,
-        animation: `floatInCircle ${Math.floor(Math.random() * 10) + 15}s linear infinite`
+        animation: `${orientation === 'bottom-top' ? 'float1' : 'float2'} ${Math.floor(Math.random() * 10) + 5}s ease-in-out infinite`
     };
 
     return <div className='star fixed rounded-full bg-slate-300' style={style} />;
@@ -24,31 +25,36 @@ const BgStars: React.FC = () => {
     const removeStars = async () => {
         const stars = document.getElementsByClassName('star');
 
-        // Fix this
         for (const star of stars) {
-            star.classList.add('fade-out');
-            setTimeout(() => {
-                star.remove();
-            }, 700);
+            star.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 700, fill: 'forwards' });
         }
+
+        return new Promise<void>((resolve) => {
+            setTimeout(() => {
+                setStars([]);
+                resolve();
+            }, 700);
+        });
     };
 
-    const changeStars = () => {
+    const changeStars = async () => {
         const { innerWidth, innerHeight } = window;
 
-        removeStars();
+        if (stars.length > 0) {
+            await removeStars();
+        }
 
-        const currStars = [];
+        const currStars: StarEl[] = [];
 
         for (let i = 0; i < 30; i++) {
             const x = Math.random() * innerWidth;
             const y = Math.random() * innerHeight;
-            const size = Math.floor(Math.random() * 3);
+            const size = Math.floor(Math.random() * 3 + 1);
 
             currStars.push({ x, y, size });
         }
 
-        setStars(currStars);
+        setStars([...currStars]);
     };
 
     useEffect(() => {
@@ -60,7 +66,7 @@ const BgStars: React.FC = () => {
     return (
         <>
             {stars.map(({ x, y, size }, i) => (
-                <Star key={i} x={x} y={y} size={size} />
+                <Star key={i} x={x} y={y} orientation={i % 2 === 0 ? 'bottom-top' : 'top-bottom'} size={size} />
             ))}
         </>
     );
